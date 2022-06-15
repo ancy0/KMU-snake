31bd14a0599c4764b146ce804ec4227b02ed717b
Eonji-sw <85453429+Eonji-sw@users.noreply.github.com>
2022-06-15 14:57:37 +0900

    Update Item.h

--- result/Item.h
+++ result/Item.h
@@ -3,71 +3,69 @@
 #include <ctime>
 #include "Rules.h"
 
-class Growth {
+class Plus {
 	int x = 0, y = 0;
 	Head *hd = nullptr; Body *bd = nullptr;
 	char pos;
 	time_t t;
 
-	// 해당 아이템의 획득 여부를 외부에 전달할 수 있는 변수
-	bool ItemState = false;
+	bool ItemState = false; // 해당 아이템을 획득했는지 여부 확인용 변수
 
 public:
-	Growth() {}
-	Growth(Head &head, Body &body);
+	Plus() {}
+	Plus(Head &head, Body &body);
 
 	int getX() { return x; }
 	int getY() { return y; }
 
-	int GrowthRule();
+	int PlusRule();
 
-	bool getItemState(){ return ItemState;};
-	void setItemState(bool b){ ItemState = b; };
+	bool getItem() {
+        return ItemState;
+    };
+	void setItem(bool check) {
+        ItemState = check;
+    };
 };
 
-
-class Poison {
+class Minus {
 	int x = 0, y = 0;
 	Head *hd = nullptr; Body *bd = nullptr;
 	char pos;
 	time_t t;
-	// 해당 아이템의 획득 여부를 외부에 전달할 수 있는 변수
-	bool ItemState = false;
-	// 만난 아이템이 성장 아이템인지(1), 독 아이템인지(0)를 판별하여 외부에 전달하는 변수
-	bool isGrowth = false;
+
+	bool ItemState = false; // 해당 아이템을 획득했는지 여부 확인용 변수
+	bool growth = false; // 아이템이 성장 아이템인지(1), 독 아이템인지(0)를 판별용 변수
 
 public:
-	Poison() {}
-	Poison(Head &head, Body &body);
+	Minus() {}
+	Minus(Head &head, Body &body);
 
 	int getX() { return x; }
 	int getY() { return y; }
 
-	int PoisonRule();
+	int MinusRule();
 
-	bool getItemState(){ return ItemState;};
-	void setItemState(bool b){ ItemState = b; };
-	};
-
+	bool getItem() {
+        return ItemState;
+    };
+	void setItem(bool check) {
+        ItemState = check;
+    };
+};
 
 class Item {
-	Growth growp;
-	Poison poisp;
+	Plus plusing;
+	Minus minusing;
 
 public:
-	Item(Growth g);
-	Item(Poison p);
+	Item(Plus p);
+	Item(Minus m);
 
-	int rule();
-	void clear();
+	int rules();
+    void clear();
 
-	// 아무 아이템을 얻지 못한 경우(0), 성장 아이템 획득 시 (1), 독 아이템 획득시(2) 출력.
-	// 모든 처리 방식이 private 변수 Growth와 Poison 클래스 내부에서 이루어지므로,
-	// 이 결과값을 외부로 전달하기 위한 함수
-	int CheckItem();
-
+    int ItemCheck();
 };
-
-
 
 #endif
