a0f1abcb643279d7afcf3a41452537d429cc3990
ancy0 <84322890+ancy0@users.noreply.github.com>
2022-06-15 13:20:24 +0900

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <chrono>
 
+#include "Snake.cpp"
+
 using namespace std;
 
 typedef struct CharPos {
@@ -20,6 +22,24 @@ int gWidth, gHeight;
 char mapData[21][60];
 vector<CharPos> wall;
 
+/* 아이템용
+int iter;
+CharPos growth_item;
+CharPos poison_item;
+*/
+
+void DrawBoard();
+// score와 mission을 나타내는 창의 공통된 내부 양식을 만든다.
+void makeFrame(WINDOW *win, const char *ch);
+
+// 점수를 기록하는 window(score, mission)
+WINDOW *score = newwin(9, 30, 1, 65);
+WINDOW *mission;
+// 미션으로 주어지는 수(bdoy length, item, poision, gate)들이 담긴 벡터
+vector<int> missions;
+
+
+
 CharPos::CharPos(int y, int x) {
     xpos = x;
     ypos = y;
@@ -32,16 +52,16 @@ CharPos::CharPos() {
 
 void InitWindow() {
     initscr(); // Curses 모드 시작
-    clear();
+    clear(); // 화면 지우기
     start_color();
     keypad(stdscr, TRUE);
-    noecho();
-    curs_set(0);
-    getmaxyx(stdscr, maxY, maxX);
+    noecho(); //  입력한 값을 화면에 출력X
+    curs_set(0); // 화면에 커서 보이지 않게 설정
+    getmaxyx(stdscr, maxY, maxX); // 화면 가로, 세로 크기 설정(표준화면이므로 window pointer에 stdscr)
 
     char message[] = "Press <Enter> to play game";
-    mvprintw(maxY/2, (maxX-strlen(message))/2, "%s", message);
-    mvchgat(maxY/2, (maxX-strlen(message))/2, -1, A_BLINK, 0, NULL);
+    mvprintw(maxY/2, (maxX-strlen(message))/2, "%s", message); // 이동한 뒤 결과 출력
+    mvchgat(maxY/2, (maxX-strlen(message))/2, -1, A_BLINK, 0, NULL); // 해당 위치부터 blinking
     
     while (c = getch()) {
         if (c == '\n') {
@@ -49,7 +69,7 @@ void InitWindow() {
             break;
         }
     }
-    getch();
+    //getch();
     move(0, 0);
     refresh(); // 실제 스크린에 출력
 }
@@ -57,14 +77,14 @@ void InitWindow() {
 void InitBoard() {
     gWidth = 60;
     gHeight = 21;
-    gameBoard = newwin(gHeight, gWidth, 0, 0);
+    gameBoard = newwin(gHeight, gWidth, 0, 0); // 새로운 윈도우 생성(사이즈, 위치)
     nodelay(gameBoard, TRUE);
     keypad(gameBoard, TRUE);
 }
 
 void DrawMap(int n) {
     int row = 0, col = 0;
-    string mapFile = "map.txt";
+    string mapFile = "Map.txt";
     ifstream map(mapFile, ios::in);
 
     init_pair(1, COLOR_WHITE, COLOR_BLACK); // 기본 배경 색 설정
@@ -72,7 +92,7 @@ void DrawMap(int n) {
     while ((c = map.get()) != EOF) {
         if (c == '2') {
             mapData[row][col++] = c;
-            wattron(gameBoard, COLOR_PAIR(1));
+            wattron(gameBoard, COLOR_PAIR(1)); // 특정 윈도우에 속성 적용
             wprintw(gameBoard, "%c", 'X');
             wattroff(gameBoard, COLOR_PAIR(1));
         }
@@ -93,7 +113,37 @@ void DrawMap(int n) {
         }
     }
     map.close();
-    wrefresh(gameBoard);
+    
+    wrefresh(gameBoard); // 특정 윈도우 새로고침
+    DrawBoard();
+}
+
+// Draw Score & Mission board
+void DrawBoard(){
+  //score board
+  score = newwin(9, 30, 1, 65);
+  wbkgd(score, COLOR_PAIR(7));
+  mvwprintw(score, 3, 6, "/   (Max Body Length)");
+  makeFrame(score, "Score Board");
+  // mission board
+  mission = newwin(9, 30, 10, 65);
+  wbkgd(mission, COLOR_PAIR(6));
+  for(int i=3; i<7; i++){
+    mvwprintw(mission, i, 7, "( )");
+  }
+  makeFrame(mission, "Mission");
+
+}
+
+// board(Score & Mission) Frame 생성.
+void makeFrame(WINDOW *win, const char *ch){
+  wborder(win, '|','|','-','-','*','*','*','*');
+  mvwprintw(win, 1, 1, ch);
+  mvwprintw(win, 3, 1, "B:");
+  mvwprintw(win, 4, 1, "+:");
+  mvwprintw(win, 5, 1, "-:");
+  mvwprintw(win, 6, 1, "G:");
+  wrefresh(win);
 }
 
 int main() {
@@ -102,6 +152,11 @@ int main() {
     InitWindow();
     InitBoard();
     DrawMap(n);
+    
+
+    Snake s;
+    s.move();
+    refresh();
 
     return 0;
 }
