99442742bda128183792b8bb07b9795b182f6dc2
ancy0 <84322890+ancy0@users.noreply.github.com>
2022-06-15 16:11:10 +0900

    Update Map.cpp

--- result/Map.cpp
+++ result/Map.cpp
@@ -1,10 +1,14 @@
+
 #include <ncurses.h>
+#include <fstream>
 #include <string>
 #include <vector>
+#include <array>
 #include "Map.h"
+using namespace std;
 
 // Gate가 생길 수 있는 wall 좌표들을 모아놓은 벡터
-vector<array<int, 2>> wall;
+vector<array<int, 2>> isWall;
 
 bool isEnd = false;
 
@@ -15,9 +19,9 @@ WINDOW *mission;
 vector<int> missions;
 
 
-void theStart(){
-  mvprintw(10, 30, "Press the any key to start game.");
-  refresh();
+void initWindow(){
+  mvprintw(10, 30, "Press <Enter> to play game");
+  refresh(); // 실제 스크린에 출력
   int key = getch();
   while(key=='\0'){}
   // 시작을 위해 키를 입력 받을때까지 기다리게 하고,
@@ -31,9 +35,7 @@ void theEnd(){
   delwin(score);
   delwin(mission);
   clear();
-  mvprintw(9, 20, "Thank you for playing this game.*^^*");
-  mvprintw(10, 20, "This game was made by Jaemin-Sim and Chaemin-Lee.");
-  mvprintw(11, 20, "KMU - Science of Software");
+  mvprintw(10, 20, "End");
   refresh();
   isEnd = true;
   clock_t delay = 3 * CLOCKS_PER_SEC;
@@ -43,124 +45,117 @@ void theEnd(){
 }
 
 // set the game base settings
-void setBase(){
-  initscr();
-  curs_set(0);  // invisible cursor
-  noecho();     // invisible keypad input
+void initSet(){
+  initscr(); // Curses 모드 시작
+  curs_set(0); // 화면에 커서 보이지 않게 설정
+  noecho();     //  입력한 값을 화면에 출력X
   start_color();
-	init_pair(1, COLOR_BLUE, COLOR_YELLOW); // background color
-	init_pair(2, COLOR_RED, COLOR_GREEN); // wall color
-	init_pair(3, COLOR_WHITE, COLOR_MAGENTA);  //head color
-	init_pair(4, COLOR_BLACK, COLOR_CYAN); // growth item color
-  init_pair(5, COLOR_CYAN, COLOR_MAGENTA); // poison item color
-	init_pair(6, COLOR_WHITE, COLOR_BLACK); // gate color
-  init_pair(7, COLOR_BLACK, COLOR_WHITE); // scoreboard color
-  init_pair(8, COLOR_MAGENTA, COLOR_BLACK); //body color
+	init_pair(1, COLOR_WHITE, COLOR_BLACK); // 기본 배경 색 설정
+	init_pair(2, COLOR_RED, COLOR_GREEN); //
+	init_pair(3, COLOR_WHITE, COLOR_MAGENTA);  //머리
+	init_pair(4, COLOR_BLACK, COLOR_CYAN); // +아이템
+  init_pair(5, COLOR_CYAN, COLOR_MAGENTA); // -아이템
+	init_pair(6, COLOR_WHITE, COLOR_BLACK); // 미션보드
+  init_pair(7, COLOR_BLACK, COLOR_WHITE); // 점수보드
+  init_pair(8, COLOR_MAGENTA, COLOR_BLACK); //몸
 
 	bkgd(COLOR_PAIR(1));
 }
 
-void DrawMap(int level){
-  // initialize the window
+void DrawMap(int stage){
   clear();
-  wall.clear();
-  attron(COLOR_PAIR(2));
-  // according to level
-  switch(level){
-    // level 1 : draw number 1
+  isWall.clear();
+  attron(COLOR_PAIR(1));
+  switch(stage){ //맵 속 벽
     case 1:
       for(int i=6; i<14; i++){
-        mvprintw(i, 10, "1");
-        mvprintw(i, 49, "1");
+        mvprintw(i, 10, "#");
+        mvprintw(i, 49, "#");
       }
       missions = {6, 2, 1, 1};
       break;
-    // level 2 : draw number 2
     case 2:
       for(int i=6; i<18; i+=4){
         for(int j=6; j<16; j++){
-          mvprintw(i, j, "1");
+          mvprintw(i, j, "#");
         }
         for(int j=44; j<54; j++){
-          mvprintw(i, j, "1");
+          mvprintw(i, j, "#");
         }
       }
-
       for(int i=7; i<10; i++){
-        mvprintw(i, 15, "1");
-        mvprintw(i, 53, "1");
+        mvprintw(i, 15, "#");
+        mvprintw(i, 53, "#");
       }
       for(int i=11; i<14; i++){
-        mvprintw(i, 6, "1");
-        mvprintw(i, 44, "1");
+        mvprintw(i, 6, "#");
+        mvprintw(i, 44, "#");
       }
       missions = {8, 3, 2, 2};
       break;
-    // level 3 : draw number 3
+
     case 3:
       for(int i=6; i<18; i+=4){
         for(int j=6; j<16; j++){
-          mvprintw(i, j, "1");
+          mvprintw(i, j, "#");
         }
         for(int j=44; j<54; j++){
-          mvprintw(i, j, "1");
+          mvprintw(i, j, "#");
         }
       }
       for(int i=7; i<14; i++){
-        mvprintw(i, 15, "1");
-        mvprintw(i, 53, "1");
+        mvprintw(i, 15, "#");
+        mvprintw(i, 53, "#");
       }
       missions = {9, 4, 3, 3};
       break;
 
-    // level 4 : draw number 4
     case 4:
       for(int i=6; i<16; i++){
-          mvprintw(11, i, "1");
+          mvprintw(11, i, "#");
       }
       for(int i=44; i<54; i++){
-          mvprintw(11, i, "1");
+          mvprintw(11, i, "#");
       }
       for(int i=6; i<11; i++){
-        mvprintw(i, 6, "1");
-        mvprintw(i, 13, "1");
-        mvprintw(i, 44, "1");
-        mvprintw(i, 51, "1");
+        mvprintw(i, 6, "#");
+        mvprintw(i, 13, "#");
+        mvprintw(i, 44, "#");
+        mvprintw(i, 51, "#");
       }
-      mvprintw(11, 13, "2");
-      mvprintw(11, 51, "2");
+      mvprintw(11, 13, "+");
+      mvprintw(11, 51, "+");
       for(int i=12; i<14; i++){
-        mvprintw(i, 13, "1");
-        mvprintw(i, 51, "1");
+        mvprintw(i, 13, "#");
+        mvprintw(i, 51, "#");
       }
       missions = {10, 5, 5, 5};
       break;
   }
-    attroff(COLOR_PAIR(2));
-//  	resize_term(HEIGHT, WIDTH);
-    // Game border
-    attron(COLOR_PAIR(2));  // border color on
+    attroff(COLOR_PAIR(1));
+    // 게임보드 
+    attron(COLOR_PAIR(1));  
     for(int i=0; i<HEIGHT; i++){
-      mvprintw(i, 0, "1");
-      mvprintw(i, WIDTH-1, "1");
+      mvprintw(i, 0, "#");
+      mvprintw(i, WIDTH-1, "#");
     }
     for(int i=0; i<WIDTH; i++){
       if((i==0)||(i==(WIDTH-1))){
-        mvprintw(0, i, "2");
-        mvprintw(HEIGHT-1, i, "2");
+        mvprintw(0, i, "+");
+        mvprintw(HEIGHT-1, i, "+");
         continue;
       }
-      mvprintw(0, i, "1");
-      mvprintw(HEIGHT-1, i, "1");
+      mvprintw(0, i, "#");
+      mvprintw(HEIGHT-1, i, "#");
     }
 
-  	attroff(COLOR_PAIR(2)); // off
-    // Gate가 생길 수 있는 wall 좌표들을 벡터에 추가
+  	attroff(COLOR_PAIR(1)); 
+    // Gate생성 가능 wall 좌표
     for(int i = 0; i < HEIGHT; i++) {
       for(int j = 0; j < WIDTH; j++)
-        if((mvinch(i, j) & A_CHARTEXT) == '1') {
+        if((mvinch(i, j) & A_CHARTEXT) == '#') {
           array<int, 2> a = {i, j};
-          wall.emplace_back(a);
+          isWall.emplace_back(a);
         }
     }
     refresh();
@@ -168,14 +163,14 @@ void DrawMap(int level){
     UpdateBoard(mission, missions);
 }
 
-// Draw Score & Mission board
+// 보드 그리기
 void DrawBoard(){
-  //score board
+  //점수보드
   score = newwin(9, 30, 1, 65);
   wbkgd(score, COLOR_PAIR(7));
-  mvwprintw(score, 3, 6, "/   (Max Body Length)");
+  mvwprintw(score, 3, 6, "/   (Max Length)");
   makeFrame(score, "Score Board");
-  // mission board
+  // 미션보드
   mission = newwin(9, 30, 10, 65);
   wbkgd(mission, COLOR_PAIR(6));
   for(int i=3; i<7; i++){
@@ -185,7 +180,7 @@ void DrawBoard(){
 
 }
 
-// board(Score & Mission) Frame 생성.
+// 미션, 점수 보드 생성.
 void makeFrame(WINDOW *win, const char *ch){
   wborder(win, '|','|','-','-','*','*','*','*');
   mvwprintw(win, 1, 1, ch);
@@ -196,7 +191,7 @@ void makeFrame(WINDOW *win, const char *ch){
   wrefresh(win);
 }
 
-// board의 적절한 위에 인자로 들어온 벡터 출력 (board의 상태를 업데이트 할때 사용)
+//  (보드 상태 업데이트 용)
 void UpdateBoard(WINDOW *board, const vector<int> &v){
     mvwprintw(board, 3, 5, " ");
     const char* body = to_string(v[0]).c_str();
@@ -207,7 +202,7 @@ void UpdateBoard(WINDOW *board, const vector<int> &v){
     mvwprintw(board, 5, 4, minus);
     const char* gate = to_string(v[3]).c_str();
     mvwprintw(board, 6, 4, gate);
-    // state 벡터의 경우, 진행 중 최대 길이도 출력해야하므로.
+    // state 벡터, 진행 중 최대 길이도 출력해야함
     if(v.size()>4){
       const char* Maxbody = to_string(v[4]).c_str();
       mvwprintw(board, 3, 8, Maxbody);
